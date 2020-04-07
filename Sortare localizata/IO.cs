658e34bd6dc38e3acbf052c5d993f09feaa693de
Feri Czenk <feri.czenk@hotmail.com>
2020-04-07 23:12:20 +0300

    Add project files.

---
+++ Sortare localizata/IO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Sortare_localizata
+{
+    internal class IO
+    {
+        internal static List<string> fileReader(string path)
+        {
+            List<string> fileLines = new List<string>();
+            string line;
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+
+                    while (true)
+                    {
+                        line = sr.ReadLine();
+                        if (line == null) break;
+                        fileLines.Add(line);
+                        //Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            return fileLines;
+        }
+
+        internal static void fileWriter(List<Person> persons,string path)
+        {
+            using(StreamWriter sw = new StreamWriter(path))
+            {
+                foreach (Person pers in persons)
+                {
+                    sw.WriteLine(pers.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Sortare localizata/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sortare_localizata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines = IO.fileReader("../../input.txt");
+            List<Person> persons = new List<Person>();
+            Console.WriteLine("Original, formated list:");
+            foreach (string line in lines)
+            {
+                Person p = lineToPerson(line);
+                persons.Add(p);
+                Console.WriteLine(p.ToString());
+
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Sorted List:");
+            persons.Sort();
+
+            for (int i = 0; i < persons.Count(); i++)
+            {
+                Console.WriteLine(persons.ElementAt(i).ToString());
+            }
+            IO.fileWriter(persons, "../../output.txt");
+            Console.WriteLine();
+        }
+
+        public static Person lineToPerson(string line)
+        {
+            char[] sep = { ' ', ';', ',', '.', ':', '!', '?', '\t', '|'};
+            string[] Lines = line.Split(sep);
+            List<string> Names = new List<string>();
+
+            foreach (string name in Lines)
+            {
+                if (!name.Equals(""))
+                {
+                    Names.Add(name);
+                }
+            }
+
+            int NamesCount = Names.Count();
+            Person p = new Person();
+            p.lastName = p.formatName(Lines[0]);
+
+            for (int i = 1; i < NamesCount; i++)
+            {
+                p.AddFirstName(Names.ElementAt(i));
+            }
+            return p;
+        }
+    }
+}
